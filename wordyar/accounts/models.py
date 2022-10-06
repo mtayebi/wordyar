6d04e698111b73097157987cbd83f3331b5f8966
mtayebi <m72tayebi@gmail.com>
2022-10-06 04:13:50 -0700

    add account model in accounts app

--- wordyar/accounts/models.py
+++ wordyar/accounts/models.py
@@ -1,3 +1,22 @@
+from email.policy import default
+import imp
+from pyexpat import model
+from statistics import mode
+from tabnanny import verbose
 from django.db import models
+from core.models import BaseModel
+from core.models import BaseUser
+from exams.models import Exam
+from django.utils.translation import gettext as _
 
-# Create your models here.
+# Create account model according to ERD
+
+class Account(BaseModel):
+    User = models.OneToOneField(BaseUser, models.CASCADE)
+    level = models.Choices(["starter", "intermediate", "advanced", "legend"])
+    exams = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    paid_user = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural, verbose_name = _("account"), _("accounts")
+    
