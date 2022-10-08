db59250253c75db0aeb59a083e72aa1b5501ee4f
mtayebi <m72tayebi@gmail.com>
2022-10-08 09:51:31 -0700

    last modification of account app

--- wordyar/exams/models.py
+++ wordyar/exams/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from accounts.models import Account
 from core.models import BaseModel
@@ -6,16 +7,19 @@ from django.utils.translation import gettext as _
 
 
 q_choice = [("0", "easy"), ("1", "normal"), ("2", "hard"),]
+
 # Create exams models including Exam, Question, Answer.
 
 class Exam(BaseModel):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    false_answer = models.IntegerField()
-    true_answer = models.IntegerField()
+    false_answer = models.IntegerField(default=0)
+    true_answer = models.IntegerField(default=0)
 
     def score(self):
-        result = (((self.true_answer*3)-self.false_answer)/((self.true_answer+self.false_answer)*3))*100
-        return result
+        if self.false_answer != 0 and self.true_answer != 0:
+            result = (((self.true_answer*3)-self.false_answer)/((self.true_answer+self.false_answer)*3))*100
+            return result
+        return 0
 
     def __str__(self) -> str:
         return f"score: {self.score()}"
