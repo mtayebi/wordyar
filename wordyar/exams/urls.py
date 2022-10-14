12f7301cae999ad48df1c0b6f4f31da30ad05a03
mtayebi <m72tayebi@gmail.com>
2022-10-14 04:07:52 -0700

    creat ajaxresponse view and api resp req for user

--- wordyar/exams/urls.py
+++ wordyar/exams/urls.py
@@ -1,10 +1,11 @@
-import imp
 from django.urls import path
-from .views import UserExam, UserExamInterface
+from .views import UserExam, UserExamInterface, AjaxResponse
 
 app_name = 'exams'
 
 urlpatterns = [
     path('exam/<int:pk>/', UserExam.as_view(), name='exam'),
     path('userexaminterface/', UserExamInterface.as_view(), name='userexaminterface'),
+    path('api/', AjaxResponse.as_view(), name='ajaxresponse'),
+
 ]
